74ac259a80e4b26eb49b6ea03297ef8c83699a26
van nguyen <nguyencvpd10794@gmail.com>
2024-09-19 19:20:09 +0700

    up

---
+++ ss6/Like.js
@@ -0,0 +1,7 @@
+// internal
+// function: hàm
+// addEventListener : lắng nghe sự kiện click
+//khi sự kiện click xảy ra thì nó sẽ thực hiện function và hiển thị alert
+document.getElementById("btn").addEventListener("click", function () {
+    alert("hello")
+})
