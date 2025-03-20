8c21cfab859f6b116a94a0345b4d0e98f299b346
van nguyen <nguyencvpd10794@gmail.com>
2025-03-20 20:18:59 +0700

    up

---
+++ Module 3/typo/script.js
@@ -0,0 +1,24 @@
+"use strict";
+let fullname = 'WanBi';
+let age = 30;
+let address = 'DakLak';
+let isMarried = true;
+let message = 'hello world';
+const students = string[] = ['minh', 'nguyen'];
+const numbers: number[] = [1,2,3,4];
+
+const person: [string, number] = ['WanBi', 30];
+let data: any;
+emun Role {
+        ADMIN,
+        CUSTOMER,
+
+}
+
+data = 'hello';
+data = 30;
+data = true;
+
+console.log(message);
+console.log(fullname, age, address, isMarried);
+console.log(students, numbers);
---
+++ Module 3/typo/script.ts
@@ -0,0 +1,8 @@
+let fullname: string = 'WanBi';
+let age: number = 30;
+let address: string = 'DakLak';
+let isMarried: boolean = true;
+let message: string = 'hello world';
+
+console.log(message)
+console.log(fullname, age, address, isMarried);
