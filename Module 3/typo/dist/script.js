b99ee2e4f59d6aef03d1a8b64727e3604ec465d3
van nguyen <nguyencvpd10794@gmail.com>
2025-03-27 19:43:46 +0700

    up

---
+++ Module 3/typo/dist/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let fullname = 'WanBi';
+let age = 30;
+let address = 'DakLak';
+let isMarried = true;
+let message = 'hello world';
+console.log(message);
+console.log(fullname, age, address, isMarried);
